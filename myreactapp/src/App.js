04f065d60794b75aa9266a259e7362b307521720
shaikarfath <shaik_arfath@columbusstate.edu>
2021-12-06 02:29:20 +0000

    commit

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Cards from "./Cards"
-import {Jumbotron} from 'reactstrap';
+import { Container, Jumbotron } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component{
